42cef9c07c2f25c7ab13409aaf2b0ffcfeeda1ed
Piyush Verma <piyus59@gmail.com>
2020-10-14 02:26:56 +0530

    Today's work Day1

--- myblog/views.py
+++ myblog/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from . models import Blog
 
 # Create your views here.
 
+
+def createblog(request):
+	if request.method == 'Post':
+		title = request.post['title']
+		text = request.post['text']
+	cats = Blog.category
+	print("===============================")
+	print(dir(cats))
+
+	return render(request, 'blog.html')
 
 def index(request):
 	return render(request, 'index.html')
